34bbef877bdb1d524cfbf0f99b89f74ab2d764bd
Venorrak <jaudet1909@gmail.com>
2024-10-22 22:38:52 -0400

    moved qte_obj.gd to right place and un-spagettified qte.gd

--- Code/Scripts/qte.gd
+++ Code/Scripts/qte.gd
@@ -1,13 +1,13 @@
 extends Node2D
-signal _minigame_finished
-signal _animation_finished
+signal _minigame_finished # sent when all actions are done
+signal _animation_finished # sent when the leaving animation is done
 
-@export var nbOfQTE: int
-@export var safeSpace: float
-@export var QTESpeed: float
-@export var separationSpace: int
-@export var GoalsY: int
-@export var topY: int
+@export var nbOfQTE: int # number of actions
+@export var safeSpace: float # safe space above and below Goals where valid
+@export var QTESpeed: float # speed at which qtes go down
+@export var separationSpace: int # separation on X between the goals
+@export var GoalsY: int # Y coordinates of the goals
+@export var topY: int # Y coordinates at which the qtes spawn
 
 @onready var Goals : Node2D = $Goals
 @onready var timer : Timer = $StartQte
@@ -15,12 +15,20 @@ signal _animation_finished
 
 var qteObj : PackedScene = preload("res://Scenes/Scenes/qteObj.tscn")
 
-var QTEs : Array = []
-var QTEIndex:int = 0
-var QTEAtIndex:int = 0
-var nbOfRight:int = 0
+var QTEs : Array = [] # list of the qtes
+var QTESpawnIndex : int = 0 # index of QTEs that the timer uses
+var QTEFocusIndex : int = 0 # index of the QTE being tracked for player input
+var nbOfRight : int = 0
+var miniGameEnded : bool = false
 var types : Array = ["A", "B", "Up", "Down", "Left", "Right"]
-var gameEnded : bool = false
+var textures : Array = [
+	load("res://Rescources/2d/AButton.png"),
+	load("res://Rescources/2d/BButton.png"),
+	load("res://Rescources/2d/arrow2.png"),
+	load("res://Rescources/2d/arrow2.png"),
+	load("res://Rescources/2d/arrow2.png"),
+	load("res://Rescources/2d/arrow2.png")
+]
 
 func _ready() -> void:
 	Goals.position.y = GoalsY
@@ -33,22 +41,14 @@ func _ready() -> void:
 	for i in nbOfQTE:
 		var newQTE : Sprite2D = qteObj.instantiate()
 		var rand : int = randi_range(0,5)
+		newQTE.texture = textures[rand]
 		match rand:
-			0:
-				newQTE.texture = load("res://Rescources/2d/AButton.png")
-			1:
-				newQTE.texture = load("res://Rescources/2d/BButton.png")
-			2:
-				newQTE.texture = load("res://Rescources/2d/arrow2.png")
 			3:
-				newQTE.texture = load("res://Rescources/2d/arrow2.png")
 				newQTE.flip_v = true
 			4:
-				newQTE.texture = load("res://Rescources/2d/arrow2.png")
 				newQTE.flip_v = true
 				newQTE.rotation_degrees = 90
 			5:
-				newQTE.texture = load("res://Rescources/2d/arrow2.png")
 				newQTE.rotation_degrees = 90
 				
 		newQTE.setType(types[rand])
@@ -60,46 +60,58 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if QTEAtIndex >= nbOfQTE:
-		if gameEnded == false:
-			gameEnded = true
+	# when game is finished
+	if QTEFocusIndex >= nbOfQTE:
+		if miniGameEnded == false:
+			miniGameEnded = true
 			_minigame_finished.emit(self)
 			AnimStateMachine.travel("exitScreen")
+			
+	
 	for qte in QTEs:
-		if GoalsY - qte.position.y < safeSpace:
-			qte.modulate = Color("ffd559", 1)
+		# change color based on if it is valid to press
+		updateQteColor(qte)
+		
+		# if qte gets out of bounds not valid anymore
 		if qte.position.y - GoalsY > safeSpace / 2:
 			showGoalResult(false, qte.getType())
 			qte.reset()
 			qte.setEnabled(false)
-			QTEAtIndex += 1
-	for type in types:
-		if Input.is_action_just_pressed(type):
-			if QTEAtIndex < nbOfQTE:
-				if QTEs[QTEAtIndex].visible == true:
+			QTEFocusIndex += 1
+	
+	# "up" "down" etc
+	if !miniGameEnded:
+		for type in types:
+			if Input.is_action_just_pressed(type):
+				if QTEs[QTEFocusIndex].getEnabled() == true:
 					var youGotIt : bool = false
-					if QTEs[QTEAtIndex].getType() == type:
-						if GoalsY - QTEs[QTEAtIndex].position.y < safeSpace:
+					# if the player got the right input
+					if QTEs[QTEFocusIndex].getType() == type:
+						#and if it was in the safe space where it is valid
+						if GoalsY - QTEs[QTEFocusIndex].position.y < safeSpace:
 							nbOfRight += 1
 							youGotIt = true
-					showGoalResult(youGotIt, QTEs[QTEAtIndex].getType())
-					QTEs[QTEAtIndex].reset()
-					QTEs[QTEAtIndex].setEnabled(false)
-					QTEAtIndex += 1
+					showGoalResult(youGotIt, QTEs[QTEFocusIndex].getType()) # red or green visual to see if they got it right
+					QTEs[QTEFocusIndex].reset()
+					QTEs[QTEFocusIndex].setEnabled(false)
+					QTEFocusIndex += 1
+
+func updateQteColor(qte) -> void:
+	if GoalsY - qte.position.y < safeSpace:
+		qte.modulate = Color("ffd559", 1)
 
 func _on_start_qte_timeout() -> void:
-	if QTEIndex < nbOfQTE:
-		if QTEs[QTEIndex].getEnabled() == true:
-			QTEs[QTEIndex].visible = true
-			QTEs[QTEIndex].setSpeed(QTESpeed)
-			QTEIndex += 1
+	if QTESpawnIndex < nbOfQTE:
+		if QTEs[QTESpawnIndex].getEnabled() == true:
+			QTEs[QTESpawnIndex].visible = true
+			QTEs[QTESpawnIndex].setSpeed(QTESpeed)
+			QTESpawnIndex += 1
 
 func showGoalResult(gotIt : bool, qteType: String) -> void:
 	#seach types array for index corresponding to qteType
 	for i in range(len(types)):
 		if types[i] == qteType:
 			Goals.get_children()[i].startAnim(gotIt)
-	pass
 
 func getScore() -> float:
 	return (nbOfRight * 10) / nbOfQTE
