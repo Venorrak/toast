5042278b27f7c183ce72335f5cb4e4dde2d16723
Venorrak <112645452+Venorrak@users.noreply.github.com>
2024-11-13 14:02:27 -0500

    leaderboard style and button

--- Code/Scripts/leaderboard_menu.gd
+++ Code/Scripts/leaderboard_menu.gd
@@ -3,10 +3,15 @@ extends Control
 var leaderboard : Array
 @export var names : VBoxContainer
 @export var scores : VBoxContainer
+@export var mainMenuButton : TextureButton
 
 func _ready() -> void:
+	mainMenuButton.grab_focus()
 	leaderboardManager.leaderboard.sortScores()
 	leaderboard = leaderboardManager.leaderboard.Leaders
 	for leaderboardIndex in range(leaderboard.size()):
 		names.get_child(leaderboardIndex + 1).text = leaderboard[leaderboardIndex].username
 		scores.get_child(leaderboardIndex + 1).text = str(leaderboard[leaderboardIndex].score)
+		
+func _on_main_menu_btn_button_up() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Scenes/main_menu.tscn")
